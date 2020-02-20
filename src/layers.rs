95a8600257f4a5cccbdb6cc407de8d7fb98ced3d
c10t <c10t@fa.ke>
2020-02-21 08:38:59 +0900

    WIP define layers

---
+++ src/layers.rs
@@ -0,0 +1,25 @@
+extern crate ndarray;
+use ndarray::prelude::*;
+
+pub trait Layer<Din, Dout: ndarray::Dimension, T> {
+  fn forward(&self, x: Array<f32, Din>) -> Array<f32, Dout>;
+  fn backwawrd(&self, x: Array<f32, Dout>) -> Array<f32, Din>;
+  fn get_params(&self) -> T;
+  fn set_params(&self, params: &T) -> ();
+}
+
+pub trait AffineParams<Din, Dout, Dw: ndarray::Dimension> {
+  fn get_weight(&self) -> Array<f32, Dw>; // Is it able to derive D3 from D1, D2?
+  fn get_bias(&self) -> Array<f32, Dout>;
+}
+
+// pub struct Affine<Din, Dout, Dw: ndarray::Dimension, P: AffineParams<Din, Dout, Dw>> {
+//   weight: Array<f32, Dw>,
+//   bias: Array<f32, Dout>,
+// }
+
+// impl<Din, Dout, Dw: ndarray::Dimension, P: AffineParams<Din, Dout, Dw>> Affine<Din, Dout, Dw, P> {
+//   fn forward(&self, x: Array<f32, Din>) -> Array<f32, Dout> {
+//     x.dot(self.weight) + self.bias
+//   }
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 extern crate ndarray;
 use ndarray::prelude::*;
 
+mod layers;
+
 fn sigmoid_f32(x: f32) -> f32 {
   1.0 / f32::exp(-x)
 }
@@ -12,6 +14,6 @@ pub fn sigmoid<D: ndarray::Dimension>(x: Array<f32, D>) -> Array<f32, D> {
 #[test]
 fn it_works() {
   let a = arr2(&[[1., 2.], [3., 4.]]);
-  // let b = arr2(&[[2.7182817, 7.389056], [20.085537, 54.59815]]);
-  assert!(a.iter().all(|x| x > &0.));
+  let b = arr2(&[[2.7182817, 7.389056], [20.085537, 54.59815]]);
+  assert!((sigmoid(a) - b).iter().all(|x| x.abs() < 0.00001));
 }
