63a33ddcb991ec731495156b734958648f629ed9
c10t <c10t@fa.ke>
2020-02-20 23:56:11 +0900

    add sigmoid test

--- src/lib.rs
+++ src/lib.rs
@@ -10,4 +10,8 @@ pub fn sigmoid<D: ndarray::Dimension>(x: Array<f32, D>) -> Array<f32, D> {
 }
 
 #[test]
-fn it_works() {}
+fn it_works() {
+  let a = arr2(&[[1., 2.], [3., 4.]]);
+  // let b = arr2(&[[2.7182817, 7.389056], [20.085537, 54.59815]]);
+  assert!(a.iter().all(|x| x > &0.));
+}
