bd9312d9a470b782d8d3bac29d85c867f36bf9c0
c10t <c10t@fa.ke>
2020-02-21 23:44:38 +0900

    add dummy fields

--- src/layers.rs
+++ src/layers.rs
@@ -1,4 +1,5 @@
 extern crate ndarray;
+use ndarray::linalg::Dot;
 use ndarray::prelude::*;
 
 pub trait Layer<Din, Dout: ndarray::Dimension, T> {
@@ -13,10 +14,12 @@ pub trait AffineParams<Din, Dout, Dw: ndarray::Dimension> {
   fn get_bias(&self) -> Array<f32, Dout>;
 }
 
-// pub struct Affine<Din, Dout, Dw: ndarray::Dimension, P: AffineParams<Din, Dout, Dw>> {
-//   weight: Array<f32, Dw>,
-//   bias: Array<f32, Dout>,
-// }
+pub struct Affine<Din, Dout, Dw: ndarray::Dimension, P: AffineParams<Din, Dout, Dw>> {
+  weight: Array<f32, Dw>,
+  bias: Array<f32, Dout>,
+  dummy_in: Array<f32, Din>,
+  dummy_params: P,
+}
 
 // impl<Din, Dout, Dw: ndarray::Dimension, P: AffineParams<Din, Dout, Dw>> Affine<Din, Dout, Dw, P> {
 //   fn forward(&self, x: Array<f32, Din>) -> Array<f32, Dout> {
